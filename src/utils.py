c945fc80e3904d6f2af305b917a0284fdcdbab3f
jamorr <55964176+jamorr@users.noreply.github.com>
2024-03-09 23:02:59 -0500

    added separate notebooks for different analysis types

--- src/utils.py
+++ src/utils.py
@@ -43,6 +43,7 @@ def get_data(
     end_date: DateTime,
     size:int=200_000,
     offset:int=0,
+    db_code:str="erm2-nwe9"
     ):
     query = f"""
     SELECT
@@ -67,7 +68,7 @@ def get_data(
     LIMIT {size}
     OFFSET {offset}
     """
-    results = connection.get("erm2-nwe9", content_type="json", query=query)
+    results = connection.get(db_code, content_type="json", query=query)
     df = pd.DataFrame.from_records(results)
     time_feats = [
         "created_date",
@@ -87,6 +88,68 @@ def get_data(
 
     return df.convert_dtypes(dtype_backend="pyarrow")
 
+
+def get_crime_data(
+    connection:Socrata,
+    start_date:DateTime,
+    size:int=200_000,
+    offset:int=0,
+    db_code:str="qgea-i56i"
+    ):
+
+    query = f"""
+    SELECT
+        cmplnt_num
+        cmplnt_fr_dt as crime_date
+        cmplnt_fr_tm
+        cmplnt_to_dt as crime_close_date
+        cmplnt_to_tm
+        rpt_dt as report_time
+        ky_cd
+        ofns_desc
+        pd_cd
+        pd_desc
+        crm_atpt_cptd_cd
+        law_cat_cd
+        boro_nm
+        loc_of_occur_desc
+        prem_typ_desc
+        juris_desc
+        jurisdiction_code
+        parks_nm
+        hadevelopt
+        housing_psa
+        susp_age_group
+        susp_race
+        susp_sex
+        patrol_boro
+        station_name
+        vic_age_group
+        vic_race
+        vic_sex
+    WHERE
+        date_extract_y(report_time)={start_date}
+    LIMIT {size}
+    OFFSET {offset}
+    """
+    results = connection.get(db_code, content_type="json", query=query)
+    df = pd.DataFrame.from_records(results)
+    time_feats = [
+        "cmplnt_to_dt",
+        "cmplnt_fr_dt",
+        "rpt_dt",
+    ]
+    numeric_feats = [
+        "latitude",
+        "longitude"
+    ]
+    for f in time_feats:
+        df[f] = pd.to_datetime(df[f], format = "ISO8601")
+
+    for f in numeric_feats:
+        df[f] = pd.to_numeric(df[f])
+
+    return df.convert_dtypes(dtype_backend="pyarrow")
 
 def load_saved_data():
     return pd.read_feather("../data/2017_1_200k.feather")
